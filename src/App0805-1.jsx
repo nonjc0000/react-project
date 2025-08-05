9ba835f68877c896209b23b3a3ef10c48c4a3239
nonjc0000 <nonjc0000@gmail.com>
2025-08-05 16:02:12 +0800

    0805

--- src/App0805-1.jsx
+++ src/App0805-1.jsx
@@ -1,6 +1,28 @@
-function App(){
-    return(
-        <h1>Hello :)</h1>
+import './css/App0805-1.css'
+
+function App() { // 字首要大寫
+    // 一般變數
+    const name = 'Joy'
+    // 屬性名稱變數
+    const myClassName = "myClass" // 屬性名稱可以是變數
+    // 方法
+    function myClick() {
+        alert('button 1');
+    }
+
+    return ( // return是最後要顯示出來的，變數資料處理寫在return前
+        // jsx fn return最外層只能回傳一個內容，所以要用空標籤整個包起來
+        <>
+            <div>
+                {/* 行內css樣式優先級比較前面 */}
+                <p style={{fontFamily: 'Helvetica', fontSize: '20px'}} className={myClassName}>姓名：{name}</p>
+                {/* 呼叫函式直接在大括號內寫函式名稱；要直接作動的話要在大括號裡寫function */}
+                <button type="button" onClick={myClick}>click 1</button>
+                <button type="button" onClick={() => {
+                    alert('button 2');
+                }}>click 2</button>
+            </div>
+        </>
     )
 }
 
---
+++ src/App0805-2.jsx
@@ -0,0 +1,111 @@
+import './css/App0805-2.css'
+
+function App() {
+    return (
+             <div className="container">
+                <h1>捷運國父紀念館美食9選！二郎系爆量拉麵、不限時貓咪咖啡廳，逛完松菸開吃。</h1>
+                <p>
+                    捷運國父紀念館站美食，逛完松菸直接開吃！國父紀念館附近除了最近盛大開幕的大巨蛋外，松菸也將舉辦達文西沉浸式展覽，周末不知道去哪，就搭捷運到國父紀念館附近逛逛，這次窩客島蒐羅了離捷運站路途10分鐘內的「9間國父紀念館站美食」，包括人氣港式餐廳、比薩店、燒肉、咖啡廳，假日約人吃美食、逛展去。
+                </p>
+
+                <img src="https://today-obs.line-scdn.net/0h5W7w8tjWangKDnp2gYMVLzJYZgk5aHBxKG4iGS4OMEwhInktYW45GycLY1R0PionKmhyHiYNY04uOyt7Nw/w1200" alt="food" />
+
+                <p>
+                    國父紀念館站附近人氣必吃「波記冰室」、「蘇阿姨比薩屋」，而二郎系拉麵「麵屋壹の穴 ICHI」也是吃過必回訪的夯店，豆芽菜推成小山的魚介豚骨拉麵，大份量CP值超高又欠拍；肉肉控想吃肉的話，有桌邊代烤的「豆町村燒肉」、韓式燒肉「這家韓式料理」等選擇；此外，近期新開幕露營風「Born to EAT本質煮藝」，以及不限時咖啡廳「Toast Chat」，必點水波蛋系列餐盤，還有超萌店貓陪你吃，捷運站美食清單全收下。
+                </p>
+
+                <img src="https://today-obs.line-scdn.net/0hknTvLTWSNEFpDyRP4oJLFlFZODBaaS5IS2ArcEwKaHUTIyASV21nIkpcYm0UPXsTSTkpL0sLPSYRNntDXA/w1200" alt="" />
+                <h2>捷運國父紀念館美食推薦：波記冰室</h2>
+
+                <p>
+                    部落客 加小菲 造訪 波記冰室 說：
+                    厚片吐司抹上花生醬沾蛋液炸到黃金酥香，接著淋上煉乳再撒上威化餅乾碎雖然甜度破表但是去港澳一定要吃這個才算道地阿!＜<a href='https://www.walkerland.com.tw/article/view/374600'>全文介紹</a>＞<br />
+                    部落客粉絲團（部落客粉絲團如無法顯示，因為受限於隱私設定）</p>
+
+                <p>
+                    #波記冰室
+                    地址：台北市大安區延吉街123號
+                    電話：02-8773-0458
+                </p>
+
+                <figure>
+                    <img src="https://today-obs.line-scdn.net/0hvEBeQT3SKWp_HDlk9JBWPUdKJRtMejNjXShvDQlLJ1wFMDk5QHx6CQ4cJEYBJW5uXyhjXwgbdlNQLTxsQw/w1200" alt="" />
+                    <figcaption>捷運國父紀念館美食推薦：波記冰室/ WalkerLand窩客島整理提供 未經許可不可轉載</figcaption>
+                </figure>
+
+                <figure>
+                    <img src="https://today-obs.line-scdn.net/0hW5U4DevJCBpoTRgU48F3TVAbBGtbKxITSnsTfR9PASpDYUxNUnxbeRlLUzYVL0oeSHgUfR8dBCJCKBhMBg/w1200" alt="" />
+                    <figcaption>捷運國父紀念館美食推薦：波記冰室/ WalkerLand窩客島整理提供 未經許可不可轉載</figcaption>
+                </figure>
+
+                <h2>捷運國父紀念館美食推薦：這家韓式料理</h2>
+                <p>部落客 洋洋的吃喝玩樂 造訪 這家韓式料理 說：
+                    別人烤的果然比自已烤的好吃，我個人偏好是五花肉，因為量不是很多也不會吃到太膩，覺得膩的時候就生菜包肉放片泡菜跟大蒜，就感覺自已在演韓劇＜<a href='https://www.walkerland.com.tw/article/view/374600'>全文介紹</a>＞<br />
+                    部落客粉絲團（部落客粉絲團如無法顯示，因為受限於隱私設定）
+                </p>
+
+                <p>
+                    #這家韓式料理
+                    地址：台北市大安區延吉街131巷18號1樓
+                    電話：02-8772-6765
+                </p>
+
+                <figure>
+                    <img src="https://today-obs.line-scdn.net/0hrAwCHFU8LW5KOj1gwbRSOXJsIR95XDdnaFkwDD1tcgxmFj44cQt-DWo9JkI0CGM-althD2k7IF0yX2Jtfw/w1200" alt="" />
+                    <figcaption>捷運國父紀念館美食推薦：這家韓式料理/ WalkerLand窩客島整理提供 未經許可不可轉載</figcaption>
+                </figure>
+
+                <figure>
+                    <img src="https://today-obs.line-scdn.net/0h1eUF7HMXbkpoP35E47IRHVBpYjtbWXRDSgwgJRk8OXpHEyEeAQ09KU4-NmZNXS1ISFkiLxg_ZygQWy8eXA/w1200" alt="" />
+                    <figcaption>捷運國父紀念館美食推薦：這家韓式料理/ WalkerLand窩客島整理提供 未經許可不可轉載</figcaption>
+                </figure>
+
+                <h2>捷運國父紀念館美食推薦：麵屋壹の穴 ICHI</h2>
+                <p>部落客 PEKO 造訪 麵屋壹の穴 ICHI 說：
+                    這高度、這份量、這價格，難怪網友說是台北二郎系拉麵中CP值最高的一家店，眼見為憑啊!!挪開最上方的蘿蔔絲，小山般的豆芽菜份量十分驚人。＜<a href='https://www.walkerland.com.tw/article/view/374600'>全文介紹</a>＞<br />
+                    部落客粉絲團（部落客粉絲團如無法顯示，因為受限於隱私設定）
+                </p>
+
+                <p>
+                    #麵屋壹の穴 ICHI
+                    地址：台北市大安區延吉街137巷22號
+                    電話：02-2711-4319
+                </p>
+
+                <figure>
+                    <img src="https://today-obs.line-scdn.net/0hH-Kc3Z5OFwJ2GgcM_ZdoVU5MG3NFfA0LVClcZVVNHDsMNgIETC5EYVNPHS5SLgcHVnRRYgAYTzFbIgQATw/w1200" alt="" />
+                    <figcaption>捷運國父紀念館美食推薦：麵屋壹の穴 ICHI/ WalkerLand窩客島整理提供 未經許可不可轉載</figcaption>
+                </figure>
+
+                <figure>
+                    <img src="https://today-obs.line-scdn.net/0hf60feM1DORkNHykXhpFGTjVJNWg-eSMQLyx2KisfYywlM3obNCtqenwXZjVwei1LLX0heH0aYi8lei0fNw/w1200" alt="" />
+                    <figcaption>捷運國父紀念館美食推薦：麵屋壹の穴 ICHI/ WalkerLand窩客島整理提供 未經許可不可轉載</figcaption>
+                </figure>
+
+                <h2>捷運國父紀念館美食推薦：蘇阿姨比薩屋</h2>
+                <p>部落客 大手牽小手。玩樂趣 造訪 蘇阿姨比薩屋 說：
+                    那個餡料真的是很敢給，起司絕對都是滿滿的，牽絲都跟瀑布一樣，裡面的肉也沒在小氣，每一口都可以吃到滿滿的肉餡，厲害！＜<a href='https://www.walkerland.com.tw/article/view/374600'>全文介紹</a>＞<br />
+                    部落客粉絲團（部落客粉絲團如無法顯示，因為受限於隱私設定）
+                </p>
+
+                <p>
+                    #蘇阿姨比薩屋
+                    地址：台北市大安區光復南路300號
+                    電話：02-2740-2328
+                </p>
+
+                <figure>
+                    <img src="https://today-obs.line-scdn.net/0hYFMuIioDBmlwDBZn-4F5PkhaChhDahxgUm8dBwcNWltYIBY-HmJVClAFXkVVORY-UGxJXF1fWlkJNEZsHg/w1200" alt="" />
+                    <figcaption>捷運國父紀念館美食推薦：蘇阿姨比薩屋/ WalkerLand窩客島整理提供 未經許可不可轉載</figcaption>
+                </figure>
+
+                <figure>
+                    <img src="https://today-obs.line-scdn.net/0hIszLKt1tFlxHNQZSzL5pC39jGi10UwxVZQNeMmFgHGRtGVEIcltFPzViQHBiAQQKZwQOOWsxGm09AVZafg/w1200" alt="" />
+                    <figcaption>捷運國父紀念館美食推薦：蘇阿姨比薩屋/ WalkerLand窩客島整理提供 未經許可不可轉載</figcaption>
+                </figure>
+            </div>
+
+    )
+}
+
+export default App;
