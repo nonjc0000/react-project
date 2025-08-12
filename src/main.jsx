b0db63c9f20260cff5e078751aaa61ce8f155cef
nonjc0000 <nonjc0000@gmail.com>
2025-08-12 15:20:57 +0800

    0812 weather

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
 // import App from './App.jsx'
-import App from './pages/App0811-weather'
+import App from './pages/App0812-weather2'
 
 createRoot(document.getElementById('root')).render(
   // 嚴謹模式
