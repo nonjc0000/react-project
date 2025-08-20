0fe2450d2aa7259a9f5ea024dc3f800f2a93054a
nonjc0000 <nonjc0000@gmail.com>
2025-08-20 14:12:08 +0800

    0820

--- src/components/CreateForm.jsx
+++ src/components/CreateForm.jsx
@@ -3,6 +3,20 @@ import React, { useState } from 'react'
 const CreateForm = ({ addTodo }) => {
     const [content, setContent] = useState('');
 
+    // 建立一個函式處理新增的todo資料
+    const addData = () => {
+        //    console.log(content.length);
+        let myInput = document.getElementById('myInput');
+        myInput.focus(); // 指定游標位置
+
+        if (content.length === 0) {
+            alert(`The input shouldn't be empty!`);
+        } else {
+            addTodo(content);
+            setContent(''); // 清除input內容
+        }
+    }
+
     // console.log(addTodo);
 
 
@@ -15,17 +29,14 @@ const CreateForm = ({ addTodo }) => {
                     placeholder='What to do next?'
                     value={content}
                     onChange={e => {
-                        setContent(e.target.value);
+                        // 去除資料前後的空白 => trim();
+                        setContent(e.target.value.trim());
                     }}
                 />
 
                 <button type='button'
-                    onClick={e => {
-                        addTodo(content);
-                        setContent(''); // 清除input內容
-                        let myInput = document.getElementById('myInput');
-                        myInput.focus(); // 指定游標位置
-                    }}>Add</button>
+                    onClick={addData}>Add
+                </button>
             </form>
             {/* <p>{content}</p> */}
         </div>
