dbefdf68dbe718092c584e3352933933fd0e6d3d
nonjc0000 <nonjc0000@gmail.com>
2025-08-20 16:37:20 +0800

    0828 toggle

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { MdDelete } from "react-icons/md";
+import { MdModeEdit } from "react-icons/md";
 
-const ToDo = ({todo}) => {
+const ToDo = ({todo, delTodo, toggleCompleted}) => {
     return (
-        <div className='todo'>
-            <p>{todo}</p>
+        // 使用字串值+三元運算子的方法控制是否套用css規則
+        <div className={`todo ${todo.isCompleted? 'completed': '' }`}>
+            <p onClick={()=>{toggleCompleted(todo.id)}}>{todo.content}</p>
+            <div>
+            <MdModeEdit 
+            style={{cursor:'pointer', marginRight:'5px'}}
+            onClick={()=>{}}
+            />
+            <MdDelete 
+            style={{cursor:'pointer'}}
+            onClick={() =>{
+                delTodo(todo.id)
+                }}/>
+            </div>
         </div>
     )
 }
--- src/components/ToDoWrapper.jsx
+++ src/components/ToDoWrapper.jsx
@@ -15,11 +15,37 @@ const ToDoWrapper = () => {
 
     const [todos, setTodos] = useState(
         [
-            { content: 'parkng fee 0820', id: Math.random() },
-            { content: 'playing guitar', id: Math.random() },
-            { content: 'cooking', id: Math.random() },
+            { content: 'parkng fee 0820', id: Math.random(), isCompleted: false },
+            { content: 'playing guitar', id: Math.random(), isCompleted: false },
+            { content: 'cooking', id: Math.random(), isCompleted: false },
         ]
     );
+
+    // 建立刪除todo
+    //傳入被刪除的todo.id
+
+    const delTodo = (id) => {
+        setTodos(todos.filter(todo => {
+            // 使用filter方法，保留不是被刪除的id
+            return todo.id !== id
+        }))
+    }
+
+    // 建立切換isCompleted屬性函式
+
+    const toggleCompleted = (id) => {
+        // isCompleted => false
+        // isCompleted => true
+        // 檢查被點擊的項目id是否跟陣列中的id一樣
+        // yes => 1. 取出todo 2. 將isCompleted屬性值反向(not)
+        // no => todo 不變
+
+        setTodos(todos.map((todo) => {
+            return todo.id === id
+                ? {...todo, isCompleted :!todo.isCompleted }
+                : todo
+        }))
+    }
 
     return (
         <div className="wrapper">
@@ -31,11 +57,13 @@ const ToDoWrapper = () => {
                 setTodos([...todos, {
                     content: newContent, // 預設一個空白內容
                     id: Math.random(),
+                    isCompleted: false,
                 }])
             }} />
+
             {
                 todos.map((todo) => {
-                    return <ToDo todo={todo.content} key={todo.id} />
+                    return <ToDo todo={todo} key={todo.id} delTodo={delTodo} toggleCompleted={toggleCompleted}/>
                 })
             }
         </div>
